e6fb9884b09ce88b34fe09c5cc2630eb185d3806
rakshit-singh2 <thisisjames007@gmail.com>
2024-10-24 12:02:29 +0530

    nodemailer

--- services/mailer.js
+++ services/mailer.js
@@ -1,5 +1,5 @@
 const sgMail = require("@sendgrid/mail");
-
+const nodemailer = require('nodemailer');
 sgMail.setApiKey(process.env.SG_KEY);
 
 const sendSGMail = async ({
@@ -11,7 +11,7 @@ const sendSGMail = async ({
   text,
 }) => {
   try {
-    const from = "a2fsharma@gmail.com";
+    const from = sender || "a2fsharma@gmail.com";
 
     const msg = {
       to: to, // Change to your recipient
@@ -22,10 +22,45 @@ const sendSGMail = async ({
       attachments,
     };
 
-    
+
     return sgMail.send(msg);
   } catch (error) {
     console.log(error);
+  }
+};
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'a2fsharma@gmail.com',  // Replace with your email
+    pass: 'rszi hekr vorq bfyj'     // Replace with your email password
+  }
+});
+// rszi hekr vorq bfyj
+const sendNodemailerMail = async ({
+  to,
+  sender,
+  subject,
+  html,
+  attachments,
+  text,
+}) => {
+  try {
+    const from = sender || "a2fsharma@gmail.com";
+
+    const mailOptions = {
+      to: to, 
+      from: from,
+      subject: subject,
+      html: html,
+      // text: text,
+      attachments,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent:', info.response);
+  } catch (error) {
+    console.error('Error sending email:', error);
   }
 };
 
@@ -33,6 +68,7 @@ exports.sendEmail = async (args) => {
   if (!process.env.NODE_ENV === "development") {
     return Promise.resolve();
   } else {
-    return sendSGMail(args);
+    // return sendSGMail(args);
+    return sendNodemailerMail(args);
   }
 };
